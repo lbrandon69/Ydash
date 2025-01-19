d8c7785b653815c8cda4a97047088242d9f3e8e8
CalvoTom <tom.calvo74@gmail.com>
2025-01-19 18:58:21 +0100

    add music

--- src/utils/music.py
+++ src/utils/music.py
@@ -1,10 +1,9 @@
 import pygame
 
-# Charger et jouer la musique de fond
 def play_music(path):
-    pygame.mixer.music.load(path)  # Charger la musique
-    pygame.mixer.music.set_volume(0.5)  # Régler le volume de la musique
-    pygame.mixer.music.play(loops=-1, start=0.0)  # Lire la musique en boucle
+    pygame.mixer.music.load(path)  
+    pygame.mixer.music.set_volume(0.5)  
+    pygame.mixer.music.play(loops=-1, start=0.0)  
 
 def stop_music():
     pygame.mixer.music.stop()
